91ad17b556af27ef40798c0acb803e1f527514cc
Glenn <gcrosby15@gmail.com>
2023-10-23 09:58:48 -0500

    add test for user token api. These tests are failing

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -9,7 +9,9 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-CREATE_USER_URL = reverse('user:create')
+CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
+
 
 def create_user(**params):
     """Create and return a new user."""
@@ -25,22 +27,22 @@ class PublicUserApiTests(TestCase):
     def test_create_user_success(self):
         """Test creating a user is successful."""
         payload = {
-            'email': 'test@example.com',
-            'password': 'testpass123',
-            'name': 'Test Name'
+            "email": "test@example.com",
+            "password": "testpass123",
+            "name": "Test Name",
         }
         result = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-        user = get_user_model().objects.get(email=payload['email'])
-        self.assertTrue(user.check_password(payload['password']))
-        self.assertNotIn('password', result.data)
+        user = get_user_model().objects.get(email=payload["email"])
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", result.data)
 
     def test_user_with_email_exists_error(self):
         payload = {
-            'email': 'test@example.com',
-            'password': 'testpass123',
-            'name': 'Test Name'
+            "email": "test@example.com",
+            "password": "testpass123",
+            "name": "Test Name",
         }
         create_user(**payload)
         result = self.client.post(CREATE_USER_URL, payload)
@@ -50,14 +52,46 @@ class PublicUserApiTests(TestCase):
     def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 characters"""
         payload = {
-            'email': 'text@example.com',
-            'password': 'pw',
-            'name': 'Test name',
+            "email": "text@example.com",
+            "password": "pw",
+            "name": "Test name",
         }
         result = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(
-            email=payload['email']
-        ).exists()
+        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test generates token for valid credentials"""
+        user_details = {
+            "name": "Test Name",
+            "email": "test@example.com",
+            "password": "test-user-password123",
+        }
+        create_user(**user_details)
+
+        payload = {"email": user_details["email"], "password": user_details["password"]}
+        result = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn("token", result.data)
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials invalid"""
+        create_user(email="test@example.com", password="goodpassword123")
+
+        payload = {"email": "test@example.com", "password": "badpass"}
+        result = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("token", result.data)
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posting a blank password returns an error."""
+        create_user(email="test@example.com", password="goodpassword123")
+        payload = {"email": "test@example.com", "password": ""}
+        result = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("token", result.data)
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
