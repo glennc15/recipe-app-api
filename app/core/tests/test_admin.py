4a8afaf5e1dc18cb0fc2ce3c704ef178ad55dc36
Glenn <gcrosby15@gmail.com>
2023-10-23 10:36:35 -0500

    Successfully added API user auth endpoint. All tests passing

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -9,40 +9,35 @@ from django.test import Client
 
 
 class AdminSiteTests(TestCase):
-
     def setUp(self):
         """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@example.com',
-            password='testpass123',
+            email="admin@example.com",
+            password="testpass123",
         )
         self.client.force_login(self.admin_user)
 
         self.user = get_user_model().objects.create_user(
-            email='user@example.com',
-            password='testpass123',
-            name='Test User'
+            email="user@example.com", password="testpass123", name="Test User"
         )
 
     def test_users_list(self):
-        url = reverse('admin:core_user_changelist')
+        url = reverse("admin:core_user_changelist")
         result = self.client.get(url)
 
         self.assertContains(result, self.user.name)
         self.assertContains(result, self.user.email)
 
     def test_edit_user_page_loads(self):
-        url = reverse('admin:core_user_change', args=[self.user.id])
+        url = reverse("admin:core_user_change", args=[self.user.id])
         result = self.client.get(url)
 
         self.assertEqual(result.status_code, 200)
 
     def test_create_user_page(self):
         """Test the create user page works"""
-        url = reverse('admin:core_user_add')
+        url = reverse("admin:core_user_add")
         result = self.client.get(url)
 
         self.assertEqual(result.status_code, 200)
-
-
