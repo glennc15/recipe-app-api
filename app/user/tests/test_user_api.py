fdefc99c5312f9605b5926a0e71086453864102f
Glenn <gcrosby15@gmail.com>
2023-10-24 10:58:07 -0500

    fix all linting errors.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -60,7 +60,9 @@ class PublicUserApiTests(TestCase):
         result = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = (
+            get_user_model().objects.filter(email=payload["email"]).exists()
+        )
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -72,7 +74,10 @@ class PublicUserApiTests(TestCase):
         }
         create_user(**user_details)
 
-        payload = {"email": user_details["email"], "password": user_details["password"]}
+        payload = {
+            "email": user_details["email"],
+            "password": user_details["password"],
+        }
         result = self.client.post(TOKEN_URL, payload)
 
         self.assertIn("token", result.data)
@@ -129,7 +134,9 @@ class PrivateUserApiTests(TestCase):
         """Test POST is not allowed for the me endpoint."""
         result = self.client.post(ME_URL, {})
 
-        self.assertEqual(result.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(
+            result.status_code, status.HTTP_405_METHOD_NOT_ALLOWED
+        )
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
