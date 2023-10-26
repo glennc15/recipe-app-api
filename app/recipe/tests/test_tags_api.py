b1a01a5b58750018a9c226fd53910b5130d9bb91
Glenn <gcrosby15@gmail.com>
2023-10-26 11:35:57 -0500

    add Tag serializer, view, and urls. Tag list tests are now passing

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -31,7 +31,7 @@ class PublicTagsApiTests(TestCase):
         """Test auth is required for retrieving tags."""
         res = self.client.get(TAGS_URL)
 
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIED)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateTagsApiTests(TestCase):
