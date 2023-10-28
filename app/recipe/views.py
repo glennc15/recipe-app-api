cca18c049b4abb8b3bf12ad2926ba7f9cdb1d135
Glenn <gcrosby15@gmail.com>
2023-10-28 16:39:40 -0500

    add image field to Recipe model

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -74,9 +74,3 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
-
-    # def get_queryset(self):
-    #     """Retrieve ingredients for authenticated users."""
-    #     return self.queryset.filter(user=self.request.user).order_by("-name")
